78c9f64ea537d178b228d2e82d8bbcce34cd78ea
mask0 <takashi.higasa@gmail.com>
2016-08-15 17:59:06 +0900

    add coment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,10 @@
-
+;;package
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
-
+;;auto-install
 (require 'auto-install)
 (auto-install-compatibility-setup)
 
+;;theme 
 (load-theme 'twilight t)
