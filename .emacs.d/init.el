554bea585754b33fe12ace7f9fd84a498e470ace
mask0 <takashi.higasa@gmail.com>
2016-08-15 17:53:09 +0900

    create .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,9 @@
+
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+
+(require 'auto-install)
+(auto-install-compatibility-setup)
+
+(load-theme 'twilight t)
