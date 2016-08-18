2fcb0a957c115aab04f612ed97366bd74b77cdb9
mask0 <takashi.higasa@gmail.com>
2016-08-18 19:02:47 +0900

    make .emacs.d and init-loader

---
+++ .emacs.d/inits/00_viewer.el
@@ -0,0 +1,20 @@
+;;not view start-up
+(setq inhibit-startup-message t)
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+(tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
