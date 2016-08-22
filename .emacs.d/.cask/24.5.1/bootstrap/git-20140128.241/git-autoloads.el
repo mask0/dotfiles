11857b881069ab5ea7065cb3b702eb6bc92ce614
mask0 <takashi.higasa@gmail.com>
2016-08-23 00:32:50 +0900

    cask init

---
+++ .emacs.d/.cask/24.5.1/bootstrap/git-20140128.241/git-autoloads.el
@@ -0,0 +1,15 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("git.el") (22459 4848 561208 176000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-autoloads.el ends here
