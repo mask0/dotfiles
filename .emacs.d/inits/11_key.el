1525fc13693b701a9137fefe73ea597709b4e085
mask0 <takashi.higasa@gmail.com>
2016-08-26 13:47:17 +0900

    rename 11_windmive.el -> 11_key.el

--- .emacs.d/inits/11_key.el
+++ .emacs.d/inits/11_key.el
@@ -1,4 +1,6 @@
+;;window move key
 (global-set-key (kbd "C-<left>") 'windmove-left)
 (global-set-key (kbd "C-<down>") 'windmove-down)
 (global-set-key (kbd "C-<up>") 'windmove-up)
 (global-set-key (kbd "C-<right>") 'windmove-right)
+
