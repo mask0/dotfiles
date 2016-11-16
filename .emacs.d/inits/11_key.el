46c271e1b37c71819d259e53329a08bd7f8a0c10
mask0 <takshi.higasa@gmail.com>
2016-11-16 21:58:20 +0900

    add line-up line-down

--- .emacs.d/inits/11_key.el
+++ .emacs.d/inits/11_key.el
@@ -4,3 +4,23 @@
 (global-set-key (kbd "C-<up>") 'windmove-up)
 (global-set-key (kbd "C-<right>") 'windmove-right)
 
+;;moveing line
+(defun move-line-down ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (forward-line)
+      (transpose-lines 1))
+    (forward-line)
+    (move-to-column col)))
+
+(defun move-line-up ()
+  (interactive)
+  (let ((col (current-column)))
+    (save-excursion
+      (forward-line)
+      (transpose-lines -1))
+    (move-to-column col)))
+
+(global-set-key (kbd "M-N") 'move-line-down)
+(global-set-key (kbd "M-P") 'move-line-up)
