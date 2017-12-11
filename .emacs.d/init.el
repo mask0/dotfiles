efae0503760eef2e880221ba2918b975ca676279
mask0 <takashi.higasa@gmail.com>
2017-12-11 22:18:30 +0900

    add minimap

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,3 +10,18 @@
 
 (setq init-loader-show-log-after-init "error-only")
 (init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(minimap-highlight-line t)
+ '(minimap-minimum-width 10)
+ '(minimap-mode t)
+ '(minimap-window-location (quote right)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
